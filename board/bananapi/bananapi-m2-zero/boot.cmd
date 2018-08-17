ff64833c58381ca2f2ef1c3e24532e722efa1312
Maik <Scholz.Maik@t-online.de>
2018-08-17 21:31:19 +0200

    system config changed to systemd

--- board/bananapi/bananapi-m2-zero/boot.cmd
+++ board/bananapi/bananapi-m2-zero/boot.cmd
@@ -1,5 +1,5 @@
 
-setenv bootargs console=ttyS0,115200 root=/dev/mmcblk0p2 rootwait consoleblank=0
+setenv bootargs console=ttyS0,115200 root=/dev/mmcblk0p2 rootwait consoleblank=0 quiet 
 
 mmc dev 0
 fatload mmc 0 $kernel_addr_r zImage
