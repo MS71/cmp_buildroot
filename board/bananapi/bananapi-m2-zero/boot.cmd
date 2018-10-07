fc0e2a8953e56a69d14553a00711d7b1318a2187
Maik <Scholz.Maik@t-online.de>
2018-10-07 20:11:33 +0200

    some changes, updated to kernel 4.18

--- board/bananapi/bananapi-m2-zero/boot.cmd
+++ board/bananapi/bananapi-m2-zero/boot.cmd
@@ -1,5 +1,5 @@
 
-setenv bootargs console=ttyS0,115200 root=/dev/mmcblk0p2 rootwait consoleblank=0 quiet 
+setenv bootargs console=ttyS0,115200 root=/dev/mmcblk0p2 rootwait consoleblank=0
 
 mmc dev 0
 fatload mmc 0 $kernel_addr_r zImage
