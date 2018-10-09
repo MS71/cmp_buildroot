8fe3891cf00afb436984de0c8ff3f040f18130c8
Maik <Scholz.Maik@t-online.de>
2018-10-09 19:24:18 +0200

    welle_io package added

---
+++ package/welle_io/welle_io.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# WELLE_IO
+#
+################################################################################
+
+WELLE_IO_VERSION = master
+WELLE_IO_SITE = $(call github,AlbrechtL,welle.io,$(WELLE_IO_VERSION))
+WELLE_IO_LICENSE = GPL-2.0+
+WELLE_IO_LICENSE_FILES = COPYING
+WELLE_IO_INSTALL_STAGING = YES
+WELLE_IO_DEPENDENCIES = faad2
+
+#WELLE_IO_CONF_OPTS += -DTHREADS_PTHREAD_ARG=OFF
+
+$(eval $(cmake-package))
