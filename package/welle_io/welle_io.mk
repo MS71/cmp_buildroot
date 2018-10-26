016cdef0c7b10bc22068eb599276d056dd691f1d
Maik <Scholz.Maik@t-online.de>
2018-10-26 19:30:43 +0200

    mpg123 added

--- package/welle_io/welle_io.mk
+++ package/welle_io/welle_io.mk
@@ -5,11 +5,12 @@
 ################################################################################
 
 WELLE_IO_VERSION = master
+#WELLE_IO_VERSION = next
 WELLE_IO_SITE = $(call github,AlbrechtL,welle.io,$(WELLE_IO_VERSION))
 WELLE_IO_LICENSE = GPL-2.0+
 WELLE_IO_LICENSE_FILES = COPYING
 WELLE_IO_INSTALL_STAGING = YES
-WELLE_IO_DEPENDENCIES = faad2
+WELLE_IO_DEPENDENCIES = faad2 mpg123
 
 WELLE_IO_CONF_OPTS += -DRTLSDR=1
 
