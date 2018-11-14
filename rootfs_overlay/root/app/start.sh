f12738999abf5d19402f06839e6d7926610400c9
Maik <Scholz.Maik@t-online.de>
2018-11-14 21:14:46 +0100

    BT mac addr generator added

--- rootfs_overlay/root/app/start.sh
+++ rootfs_overlay/root/app/start.sh
@@ -44,14 +44,21 @@ sync
 mount /tmp/app -o remount,rw
 sync
 
-# enable BT6212 ...
+# enable Bluetooth AP6212 ...
 devmem2 0x1f00060 b 1
 echo 204 > /sys/class/gpio/export
 echo out > /sys/class/gpio/gpio204/direction
 echo 0 > /sys/class/gpio/gpio204/value
 echo 1 > /sys/class/gpio/gpio204/value
 sleep 0.1
-/usr/bin/hciattach /dev/ttyS1 bcm43xx 1500000 flow bdaddr 43:29:B1:55:01:01
+# generate BT MAC
+export BTADDR_FILE=/tmp/app/bt.addr                                      
+if [ -f $BTADDR_FILE ]; then                                               
+ echo ""                                                                       
+else                                                                           
+ tr -dc A-F0-9 < /dev/urandom | head -c 10 | sed -r 's/(..)/\1:/g;s/:$//;s/^/02:/' > $BTADDR_FILE                                                            
+fi   
+/usr/bin/hciattach /dev/ttyS1 bcm43xx 1500000 flow bdaddr `cat $BTADDR_FILE` 
 hciconfig hci0 sspmode 0
 hciconfig hci0 up
 bt-agent -c NoInputNoOutput -p /etc/bluetooth/pins.cfg -d
