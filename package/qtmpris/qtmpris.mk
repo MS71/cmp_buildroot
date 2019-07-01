cce4c7112d02589b5aa16dc7ebcd1a6616366c5a
MS <Scholz.Maik@t-online.de>
2019-07-01 22:11:16 +0200

    version changed

--- package/qtmpris/qtmpris.mk
+++ package/qtmpris/qtmpris.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-QTMPRIS_VERSION = 5d31ba54f08f46ae466a638a66ffc68b37d353f4
+QTMPRIS_VERSION = 1.0.0
 #QTMPRIS_SITE = $(call github,nemomobile,qtmpris,$(QTMPRIS_VERSION))
 QTMPRIS_SITE = https://git.merproject.org/mer-core/qtmpris
 #QTMPRIS_SITE = $(call git.merproject.org,mer-core,qtmpris,$(QTMPRIS_VERSION))
@@ -29,13 +29,13 @@ define QTMPRIS_INSTALL_STAGING_CMDS
 endef
 
 define QTMPRIS_INSTALL_TARGET_CMDS
-    cp -dpf $(BUILD_DIR)/qtmpris-origin_master/src/libmpris-qt5.so* $(STAGING_DIR)/usr/lib
-    cp -dpf $(BUILD_DIR)/qtmpris-origin_master/src/pkgconfig/*.pc $(STAGING_DIR)/usr/lib/pkgconfig/.
+    cp -dpf $(BUILD_DIR)/qtmpris-1.0.0/src/libmpris-qt5.so* $(STAGING_DIR)/usr/lib
+    cp -dpf $(BUILD_DIR)/qtmpris-1.0.0/src/pkgconfig/*.pc $(STAGING_DIR)/usr/lib/pkgconfig/.
     mkdir -p $(STAGING_DIR)/usr/include/qt5/MprisQt
-    cp -dpf $(BUILD_DIR)/qtmpris-origin_master/src/MprisQt $(STAGING_DIR)/usr/include/qt5/MprisQt/MprisQt
-    cp -dpf $(BUILD_DIR)/qtmpris-origin_master/src/Mpris $(STAGING_DIR)/usr/include/qt5/MprisQt/Mpris
-    cp -dpf $(BUILD_DIR)/qtmpris-origin_master/src/MprisPlayer $(STAGING_DIR)/usr/include/qt5/MprisQt/.
-    cp -dpf $(BUILD_DIR)/qtmpris-origin_master/src/*.h $(STAGING_DIR)/usr/include/qt5/MprisQt/.
+    cp -dpf $(BUILD_DIR)/qtmpris-1.0.0/src/MprisQt $(STAGING_DIR)/usr/include/qt5/MprisQt/MprisQt
+    cp -dpf $(BUILD_DIR)/qtmpris-1.0.0/src/Mpris $(STAGING_DIR)/usr/include/qt5/MprisQt/Mpris
+    cp -dpf $(BUILD_DIR)/qtmpris-1.0.0/src/MprisPlayer $(STAGING_DIR)/usr/include/qt5/MprisQt/.
+    cp -dpf $(BUILD_DIR)/qtmpris-1.0.0/src/*.h $(STAGING_DIR)/usr/include/qt5/MprisQt/.
     cp -dpf $(STAGING_DIR)/usr/lib/libmpris-qt5.so* $(TARGET_DIR)/usr/lib
 endef
 
