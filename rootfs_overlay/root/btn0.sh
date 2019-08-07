6e9f6e61f8f933223773a9e2a5d18d271deef7ba
MS <Scholz.Maik@t-online.de>
2019-08-07 22:08:17 +0200

    bt script changed

--- rootfs_overlay/root/btn0.sh
+++ rootfs_overlay/root/btn0.sh
@@ -1,8 +1,1 @@
-#!/bin/sh
-BOOTCNTFN=/data/boot.cnt 
-if [ -f $BOOTCNTFN ]; then
- BOOTCNT=`cut -d ',' -f2 $BOOTCNTFN`
- mkdir -p /data/log
- hcidump -w /data/log/${BOOTCNT}_hci.cap
-fi
-
+#!/bin/bash
--- rootfs_overlay/root/btn1.sh
+++ rootfs_overlay/root/btn1.sh
@@ -3,6 +3,6 @@ BOOTCNTFN=/data/boot.cnt
 if [ -f $BOOTCNTFN ]; then
  BOOTCNT=`cut -d ',' -f2 $BOOTCNTFN`
  mkdir -p /data/log
- hcidump -w /data/log/$BOOTCNTFN_hci.cap
+ hcidump -w /data/log/${BOOTCNT}_hci.cap
 fi
 
